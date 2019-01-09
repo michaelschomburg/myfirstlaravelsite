6d363004a9c3712c2698ea9cd270642101093ec3
Michael <schomburg.michael90@gmail.com>
2019-01-09 23:04:45 +0100

    Added a basic route and linked it from the homepage - Video: Basic routing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
+                <a href="/contact"> click here for contact</a>
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
