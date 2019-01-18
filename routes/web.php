cd285bfad82df8c5e7bc02cc7db373f915cf8e8c
Michael <michael.schomburg@studydrive.com>
2019-01-18 17:04:45 +0100

    finished tutorial

--- routes/web.php
+++ routes/web.php
@@ -10,36 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'PagesController@home');
+Route::get('/about', 'PagesController@about');
+Route::get('/contact', 'PagesController@contact');
 
-Route::get('/', function () {
-    return view('welcome', [
-     'foo' => 'foobar',
-     'tasks' => [
-         'Go to the store',
-         'Go to the market',
-         'Go to work',
-         'Go to the concert'
-     ]
-    ]);
-
-    /*  $tasks = [
-        'Go to the store',
-        'Go to the market',
-        'Go to work',
-        'Go to the concert'
-    ];
-    return view('welcome')->withTasks($tasks)->withFoo('foo');
-
-    /*return view('welcome', [
-        'tasks' => $tasks,
-        'foo' => 'foobar'
-    ]);*/
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
