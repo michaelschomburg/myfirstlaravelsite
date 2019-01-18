163eece2ce920a34fd4d536648c863c3bae3b795
Michael <michael.schomburg@studydrive.com>
2019-01-18 15:21:00 +0100

    deleted unused part

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,6 @@
 @extends('layout')
 
 @section('content')
-    {{--<h1>My First Website</h1>
-    <ul>
-        <?php foreach ($tasks as $task) : ?>
-            <li><?= $task; ?></li>
-        <?php endforeach; ?>
-    </ul>--}}
     <h1>My First {{$foo}} Website</h1>
     <ul>
         @foreach($tasks as $task)
